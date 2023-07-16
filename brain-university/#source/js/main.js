78354664bc86cc789138cb089633729b0a766e38
Nikita <ghostwarriorn@gmail.com>
2023-07-16 13:46:56 +0500

    delete source

--- brain-university/#source/js/main.js
+++
@@ -1,6 +0,0 @@
-//@prepros-append jq-start.js
-//@prepros-append forms.js
-//@prepros-append script.js
-//@prepros-append responsive.js
-//@prepros-append sliders.js
-//@prepros-append jq-end.js
--- creatives/#source/js/main.js
+++
@@ -1,5 +0,0 @@
-//@prepros-append jq-start.js
-//@prepros-append forms.js
-//@prepros-append script.js
-//@prepros-append responsive.js
-//@prepros-append jq-end.js
